4670c03e766ed1cb1c45c1bfef47fc0697bedc73
Andy Bart <abartholomew2903@gmail.com>
2024-01-19 15:23:03 +0000

    adding onto calculator

--- calculator.py
+++ calculator.py
@@ -1,8 +1,25 @@
+operator = input('''
+Please type the math operation you wish to calculate: 
++ for addition, 
+- for subtraction, 
+* for mutlipilication, 
+% for division.
+: ''')
+
 num1 = int(input('Enter number: '))
 num2 = int(input('Enter number: '))
 
 def calculator(num1, num2):
-    print(num1 * num2)
+    if operator == "+":
+        return num1 + num2
+    elif operator == "-":
+        return num1 - num2
+    elif operator == "*":
+        return num1 * num2
+    elif operator == "%":
+        return num1 % num2
+    else:
+        return "You have not used a correct operator"
 
-calculator(num1,num2)
+print("The Answer Is ", calculator(num1, num2))
 
